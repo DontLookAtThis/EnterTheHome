a739c8d6f73f0d5953aefe8716a11f7432d231be
Michael <xumanpro@hotmail.com>
2019-01-25 20:20:17 +1300

    -Blank C++ unreal project EnterTheHome Created

---
+++ Source/EnterTheHome/EnterTheHomeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EnterTheHomeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENTERTHEHOME_API AEnterTheHomeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
